2d5f913984428519ce85c2431805c686e12799a4
Stella Song <52116178+BasilissaOfNuts@users.noreply.github.com>
2026-05-03 23:36:12 -0400

    Good luck figuring this out!

--- core/utils/Signal.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtCore import QObject, pyqtSignal
-
-class SignalBus(QObject):
-    # This signal tells every widget to re-fetch their data from the DB
-    refresh_requested = pyqtSignal()
-
-# Singleton instance
-global_signal = SignalBus()
